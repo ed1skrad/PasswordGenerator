ee9c77dd6ae1a082d5de1211332058f86c16252a
ed1skrad <ediskrad11111@gmail.com>
2024-03-05 11:42:26 +0300

    Clear code smelles

--- src/main/java/com/password/generator/bsuir/security/repository/UserRepository.java
+++ src/main/java/com/password/generator/bsuir/security/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.password.generator.bsuir.security.repository;
 
 import com.password.generator.bsuir.security.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/main/java/com/password/generator/bsuir/security/service/AuthenticationService.java
+++ src/main/java/com/password/generator/bsuir/security/service/AuthenticationService.java
@@ -18,9 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AuthenticationService {
