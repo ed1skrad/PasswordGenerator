71b55bb12f4170f80b14d3fb60c9488606efaabc
ed1skrad <ediskrad11111@gmail.com>
2024-03-17 21:53:29 +0300

    UI changes

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -4,36 +4,122 @@ import '../../css/AdminPanel/adminPanel.css';
 import API_URL from "../../config/config";
 
 const AdminPanel = () => {
-    const [list, setList] = useState([]);
+    const [passwords, setPasswords] = useState([]);
+    const [username, setUsername] = useState('');
+    const [displayMode, setDisplayMode] = useState('all');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.get(`${API_URL}/api/password/all`);
-            localStorage.setItem('token', response.data.token);
-            window.location.href = '/password';
-
-            const passwords = response.data.map(item => ({
-                passwordId: item.id,
-                password: item.password,
-                username: item.user.username
-            }));
-
-            setList(passwords);
-        } catch (error) {
-            console.error('Failed to login: ', error);
-        }
+    const handleGetAllPasswords = () => {
+        axios.get(`${API_URL}/api/password/all`)
+            .then(response => {
+                setPasswords(response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     };
+
+    const handleGetUserPasswords = () => {
+        axios.get(`${API_URL}/api/password/user/${username}/passwords`)
+            .then(response => {
+                setPasswords(response.data);
+            })
+            .catch(error => {
+                if (error.response && error.response.status === 404) {
+                    setPasswords([]);
+                } else {
+                    console.error('Error:', error);
+                }
+            });
+    };
+
+    const handleChangeUsername = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handleDisplayAllPasswords = () => {
+        setDisplayMode('all');
+        handleGetAllPasswords();
+    };
+
+    const handleDisplayUserPasswords = () => {
+        setDisplayMode('user');
+        handleGetUserPasswords();
+    };
+
+    const handleDisplayAllUsers = () => {
+        const token = localStorage.getItem('token');
+        axios.get(`${API_URL}/api/users`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => {
+                setPasswords(response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await axios.delete(`${API_URL}/api/password/delete/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    const handleEdit = () =>{
+        
+    }
+
+    const deleteAll = () => {
+        const confirmed = window.confirm('Вы уверены, что хотите удалить все?');
+        if (confirmed) {
+            axios.delete(`${API_URL}/api/password/deleteAllBulkPasswords`)
+                .then(response => {
+                    console.log('Response', response.data);
+                    alert('Успешно удалено')
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                })
+        }
+    }
 
     return (
         <div>
-            {list.map(item => (
-                <div key={item.passwordId}>
-                    <h3>Имя пользователя: {item.username}</h3>
-                    <p>ID пароля: {item.passwordId}</p>
-                    <p>Пароль: {item.password}</p>
-                </div>
-            ))}
+            <div>
+                <button onClick={handleDisplayAllPasswords} className='button-main'>Display All Passwords</button>
+                <button onClick={handleDisplayUserPasswords} className='button-main'>Display User Passwords</button>
+                <button onClick={handleDisplayAllUsers} className='button-main'>Display All Users</button>
+                {displayMode === 'user' && (
+                    <input type="text" value={username} onChange={handleChangeUsername} />
+                )}
+            </div>
+            <div className="list-container">
+                {passwords.length === 0 ? (
+                    <p>No passwords found.</p>
+                ) : (
+                    passwords.map(passwordObj => (
+                        <div key={passwordObj.id} className='pass-block'>
+                            <span>Password: {passwordObj.password}</span>
+                            <span>Difficulty: {passwordObj.difficulty}</span>
+                            <span>User: {passwordObj.user.username}</span>
+                            <span>E-mail: {passwordObj.user.email}</span>
+                            <button>Edit</button>
+                            <button onClick={() => handleDelete(passwordObj.id)}>Delete</button>
+                        </div>
+                    ))
+                )}
+            </div>
+            <button onClick={deleteAll} className='button-main'>Delete All</button>
         </div>
     );
 };
