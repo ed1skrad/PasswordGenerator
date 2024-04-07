e076a5d7905c14fae46027c0da940b5a1944e6e1
ed1skrad <ediskrad11111@gmail.com>
2024-04-07 21:16:59 +0300

    Deprecated method fixed

--- src/main/java/com/password/generator/bsuir/security/service/JwtService.java
+++ src/main/java/com/password/generator/bsuir/security/service/JwtService.java
@@ -116,7 +116,11 @@ public class JwtService {
      * @return the claims.
      */
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     /**
