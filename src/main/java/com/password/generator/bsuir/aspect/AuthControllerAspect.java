bdafe75e432fab035b4814c4a11a297544624c14
ed1skrad <ediskrad11111@gmail.com>
2024-04-07 14:05:24 +0300

    Auth controller aspect enabled

---
+++ src/main/java/com/password/generator/bsuir/aspect/AuthControllerAspect.java
@@ -0,0 +1,50 @@
+package com.password.generator.bsuir.aspect;
+
+import com.password.generator.bsuir.security.controller.AuthController;
+import com.password.generator.bsuir.security.domain.dto.SignInRequest;
+import com.password.generator.bsuir.security.domain.dto.SignUpRequest;
+import java.util.Arrays;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+/**
+ * This aspect logs all method calls in the {@link AuthController} class.
+ */
+
+@Aspect
+@Component
+public class AuthControllerAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthControllerAspect.class);
+
+    /**
+     * Logs a message before each method call in the {@link AuthController} class.
+     *
+     * @param joinPoint the join point representing the method call
+     */
+    @Before("execution(* com.password.generator.bsuir.security.controller.AuthController.*(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        LOGGER.info("Calling method: {} with arguments: {}", methodName, Arrays.toString(args));
+        switch (methodName) {
+            case "signUp" -> {
+                SignUpRequest signUpRequest = (SignUpRequest) args[0];
+                LOGGER.info("Signing up user: {}", signUpRequest.getUsername());
+            }
+            case "signIn" -> {
+                SignInRequest signInRequest = (SignInRequest) args[0];
+                LOGGER.info("Signing in user: {}", signInRequest.getUsername());
+            }
+            case "logout" -> {
+                HttpServletRequest request = (HttpServletRequest) args[0];
+                String username = (String) request.getAttribute("username");
+                LOGGER.info("Logging out user: {}", username);
+            }
+        }
+    }
+}
